c1b6437d95b86f5547898361d2db86160cd538f0
tejasreddy2006 <prtejasreddy2006@gmail.com>
2025-08-19 19:44:28 +0530

    Update goodbyeSpeaker.js

--- module-04-solution/goodbyeSpeaker.js
+++ module-04-solution/goodbyeSpeaker.js
@@ -1,1 +1,11 @@
+(function (window) {
+  var goodbyeSpeaker = {};
+  var speakWord = "Goodbye";
+
+  goodbyeSpeaker.speak = function (name) {
+    console.log(speakWord + " " + name);
+  };
+
+  window.goodbyeSpeaker = goodbyeSpeaker;
+})(window);
 
